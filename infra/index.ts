c18a3b9c0e02eee50187dad09a1e1612d43da20d
Ido Shamun <ido@daily.dev>
2021-11-11 12:49:18 +0200

    ci: deploy to k8s (#214)

--- infra/index.ts
+++ infra/index.ts
@@ -1,25 +1,35 @@
 import * as gcp from '@pulumi/gcp';
 import {
-    CloudRunAccess,
-    config, createCloudRunService, createEnvVarsFromSecret,
-    createK8sServiceAccountFromGCPServiceAccount, createMigrationJob,
-    createServiceAccountAndGrantRoles, getCloudRunPubSubInvoker,
-    imageTag, infra, k8sServiceAccountToIdentity, createCronJobs
+  CloudRunAccess,
+  config,
+  createCloudRunService,
+  createEnvVarsFromSecret,
+  createK8sServiceAccountFromGCPServiceAccount,
+  createMigrationJob,
+  createServiceAccountAndGrantRoles,
+  getCloudRunPubSubInvoker,
+  infra,
+  k8sServiceAccountToIdentity,
+  createCronJobs,
+  getImageTag,
+  createKubernetesSecretFromRecord,
+  createAutoscaledExposedApplication, convertRecordToContainerEnvVars, getMemoryAndCpuMetrics,
 } from '@dailydotdev/pulumi-common';
-import {Output} from '@pulumi/pulumi';
+import { Input, Output } from '@pulumi/pulumi';
 
+const imageTag = getImageTag();
 const name = 'monetization';
 
 const vpcConnector = infra.getOutput('serverlessVPC') as Output<gcp.vpcaccess.Connector>;
 
-const {serviceAccount} = createServiceAccountAndGrantRoles(
-    `${name}-sa`,
-    name,
-    `daily-${name}`,
-    [
-        {name: 'trace', role: 'roles/cloudtrace.agent'},
-        {name: 'secret', role: 'roles/secretmanager.secretAccessor'},
-    ],
+const { serviceAccount } = createServiceAccountAndGrantRoles(
+  `${name}-sa`,
+  name,
+  `daily-${name}`,
+  [
+    { name: 'trace', role: 'roles/cloudtrace.agent' },
+    { name: 'secret', role: 'roles/secretmanager.secretAccessor' },
+  ],
 );
 
 const secrets = createEnvVarsFromSecret(name);
@@ -27,60 +37,67 @@ const secrets = createEnvVarsFromSecret(name);
 const image = `gcr.io/daily-ops/daily-${name}:${imageTag}`;
 
 // Create K8S service account and assign it to a GCP service account
-const {namespace} = config.requireObject<{ namespace: string }>('k8s');
+const { namespace } = config.requireObject<{ namespace: string }>('k8s');
 
 const k8sServiceAccount = createK8sServiceAccountFromGCPServiceAccount(
-    `${name}-k8s-sa`,
-    name,
-    namespace,
-    serviceAccount,
+  `${name}-k8s-sa`,
+  name,
+  namespace,
+  serviceAccount,
 );
 
 new gcp.serviceaccount.IAMBinding(`${name}-k8s-iam-binding`, {
-    role: 'roles/iam.workloadIdentityUser',
-    serviceAccountId: serviceAccount.id,
-    members: [k8sServiceAccountToIdentity(k8sServiceAccount)],
+  role: 'roles/iam.workloadIdentityUser',
+  serviceAccountId: serviceAccount.id,
+  members: [k8sServiceAccountToIdentity(k8sServiceAccount)],
 });
 
 const migrationJob = createMigrationJob(
-    `${name}-migration`,
-    namespace,
-    image,
-    ['/main', 'migrate'],
-    secrets,
-    k8sServiceAccount,
+  `${name}-migration`,
+  namespace,
+  image,
+  ['/main', 'migrate'],
+  secrets,
+  k8sServiceAccount,
 );
+
+const limits: Input<{
+  [key: string]: Input<string>;
+}> = {
+  cpu: '1',
+  memory: '256Mi',
+};
 
 // Deploy to Cloud Run (foreground & background)
 const service = createCloudRunService(
-    name,
-    image,
-    secrets,
-    {cpu: '1', memory: '256Mi'},
-    vpcConnector,
-    serviceAccount,
-    {
-        minScale: 1,
-        concurrency: 250,
-        dependsOn: [migrationJob],
-        access: CloudRunAccess.Public,
-        iamMemberName: `${name}-public`,
-    },
+  name,
+  image,
+  secrets,
+  limits,
+  vpcConnector,
+  serviceAccount,
+  {
+    minScale: 1,
+    concurrency: 250,
+    dependsOn: [migrationJob],
+    access: CloudRunAccess.Public,
+    iamMemberName: `${name}-public`,
+  },
 );
 
 const bgService = createCloudRunService(
-    `${name}-background`,
-    image,
-    secrets,
-    {cpu: '1', memory: '256Mi'},
-    vpcConnector,
-    serviceAccount,
-    {
-        dependsOn: [migrationJob],
-        access: CloudRunAccess.PubSub,
-        iamMemberName: `${name}-pubsub-invoker`,
-        args: ['background'],
-    },
+  `${name}-background`,
+  image,
+  secrets,
+  { cpu: '1', memory: '256Mi' },
+  vpcConnector,
+  serviceAccount,
+  {
+    dependsOn: [migrationJob],
+    access: CloudRunAccess.PubSub,
+    iamMemberName: `${name}-pubsub-invoker`,
+    args: ['background'],
+  },
 );
 
 export const serviceUrl = service.statuses[0].url;
@@ -88,40 +105,78 @@ export const bgServiceUrl = bgService.statuses[0].url;
 
 const cloudRunPubSubInvoker = getCloudRunPubSubInvoker();
 
-new gcp.pubsub.Subscription(`${name}-sub-views`, {
-    topic: 'views',
-    name: `${name}-views`,
-    pushConfig: {
-        pushEndpoint: bgServiceUrl.apply((url) => `${url}/view`),
-        oidcToken: {
-            serviceAccountEmail: cloudRunPubSubInvoker.email,
-        }
+const envVars = config.requireObject<Record<string, string>>('env');
+
+createKubernetesSecretFromRecord({
+  data: envVars,
+  resourceName: 'k8s-secret',
+  name,
+  namespace,
+});
+
+createAutoscaledExposedApplication({
+  name,
+  namespace: namespace,
+  version: imageTag,
+  serviceAccount: k8sServiceAccount,
+  containers: [
+    {
+      name: 'app',
+      image,
+      ports: [{ name: 'http', containerPort: 3000, protocol: 'TCP' }],
+      readinessProbe: {
+        httpGet: { path: '/health', port: 'http' },
+      },
+      env: [
+        ...convertRecordToContainerEnvVars({ secretName: name, data: envVars }),
+        { name: 'PORT', value: '3000' },
+      ],
+      resources: {
+        requests: limits,
+        limits,
+      },
     },
-    retryPolicy: {
-        minimumBackoff: '10s',
-        maximumBackoff: '600s',
-    }
+  ],
+  maxReplicas: 10,
+  metrics: getMemoryAndCpuMetrics(),
+});
+
+new gcp.pubsub.Subscription(`${name}-sub-views`, {
+  topic: 'views',
+  name: `${name}-views`,
+  pushConfig: {
+    pushEndpoint: bgServiceUrl.apply((url) => `${url}/view`),
+    oidcToken: {
+      serviceAccountEmail: cloudRunPubSubInvoker.email,
+    },
+  },
+  labels: { app: name },
+  retryPolicy: {
+    minimumBackoff: '10s',
+    maximumBackoff: '600s',
+  },
 });
 
 new gcp.pubsub.Subscription(`${name}-sub-new-ad`, {
-    topic: 'ad-image-processed',
-    name: `${name}-new-ad`,
-    pushConfig: {
-        pushEndpoint: bgServiceUrl.apply((url) => `${url}/newAd`),
-        oidcToken: {
-            serviceAccountEmail: cloudRunPubSubInvoker.email,
-        }
+  topic: 'ad-image-processed',
+  name: `${name}-new-ad`,
+  pushConfig: {
+    pushEndpoint: bgServiceUrl.apply((url) => `${url}/newAd`),
+    oidcToken: {
+      serviceAccountEmail: cloudRunPubSubInvoker.email,
     },
-    retryPolicy: {
-        minimumBackoff: '10s',
-        maximumBackoff: '600s',
-    },
-    expirationPolicy: {
-        ttl: '',
-    },
+  },
+  labels: { app: name },
+  retryPolicy: {
+    minimumBackoff: '10s',
+    maximumBackoff: '600s',
+  },
+  expirationPolicy: {
+    ttl: '',
+  },
 });
 
 createCronJobs(name, [{
-    name: 'delete-old-tags',
-    schedule: '6 10 * * 0',
+  name: 'delete-old-tags',
+  schedule: '6 10 * * 0',
 }], bgServiceUrl);
